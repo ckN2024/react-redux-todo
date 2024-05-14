b96dc523bbf1c998f17d4e858a286082e702bf2f
ck <ck@tyloones.com>
2024-05-14 11:04:23 +0530

    Update and Keep existing buttons removed, update on click implemented

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,9 +12,6 @@ import stringFormatter from "../helpers/stringFormatter.jsx";
 
 const Todo = ({ todo }) => {
   const date = new Date(todo.createdAt);
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
 
   const [isEditModeOn, setIsEditModeOn] = useState(false);
   const [isTitleFilled, setIsTitleFilled] = useState(true);
@@ -28,13 +25,22 @@ const Todo = ({ todo }) => {
   // const [formattedDescription, setFormattedDescription] = useState("")
 
   const formattedDescription = stringFormatter(todo.description);
-  console.log(formattedDescription)
 
   const dispatch = useDispatch();
+
+
+  const editAndSubmitHandler = (e) => {
+    if(isEditModeOn) {
+      editTodoSubmitHandler(e)
+    } else {
+      setIsEditModeOn(!isEditModeOn)
+    }
+  }
 
   const editTodoSubmitHandler = (e) => {
     e.preventDefault();
 
+    // if(isEditModeOn) {
     editedTodo.title = editedTodo.title.trim();
     editedTodo.description = editedTodo.description.trim();
 
@@ -44,110 +50,125 @@ const Todo = ({ todo }) => {
     }
 
     dispatch(editTodo(editedTodo));
+    setIsEditModeOn(false);
+    // }
   };
 
-  const todoStateHandler = () => {
+  const todoStateHandler = (e) => {
+    e.stopPropagation()
     dispatch(changeTodoCompletedState(todo.createdAt));
   };
 
   return (
-    <div className="hover:bg-gray-100 shadow p-2 flex gap-[2em] rounded-lg">
+    <div
+      onClick={editAndSubmitHandler} 
+      className="hover:bg-gray-100 shadow p-2 flex gap-[2em] rounded-lg"
+    >
       <input
         type="checkbox"
         className="min-w-4 min-h-4 my-auto"
-        onClick={todoStateHandler}
+        onClick={(e) => todoStateHandler(e)}
       />
       <div className="grow">
         {isEditModeOn ? (
           // for editing a todo
-          <form
-            className="flex flex-col items-center gap-1"
-            onSubmit={editTodoSubmitHandler}
-          >
-            <div className="flex flex-col w-full">
-              <input
-                type="text"
-                placeholder="title"
-                value={editedTodo.title}
+          <div className="flex gap-2">
+            <div
+              className="flex flex-col items-center gap-1 grow"
+              // onClick={editTodoSubmitHandler}
+            >
+              <div className="flex flex-col w-full">
+                <input
+                  type="text"
+                  placeholder="title"
+                  value={editedTodo.title}
+                  onClick={(e)=> e.stopPropagation()}
+                  onChange={(e) => {
+                    setEditedTodo({ ...editedTodo, title: e.target.value });
+                    setIsTitleFilled(true);
+                  }}
+                  className={`border px-3 py-[0.3em] rounded-[15px] focus:outline-none focus:border-green-500 w-full ${
+                    editedTodo.title.trim() === "" ? "focus:border-red-500" : ""
+                  }`}
+                />
+                {isTitleFilled ? (
+                  ""
+                ) : (
+                  <p className="text-red-500 text-sm">* Title is mandatory</p>
+                )}
+              </div>
+              <textarea
+                rows="2"
+                placeholder="Description"
+                value={editedTodo.description}
+                onClick={(e)=>{e.stopPropagation()}}
                 onChange={(e) => {
-                  setEditedTodo({ ...editedTodo, title: e.target.value });
-                  setIsTitleFilled(true);
+                  setEditedTodo({ ...editedTodo, description: e.target.value });
                 }}
-                className={`border px-3 py-[0.3em] rounded-[15px] focus:outline-none focus:border-green-500 w-full ${
-                  editedTodo.title.trim() === "" ? "focus:border-red-500" : ""
-                }`}
-              />
-              {isTitleFilled ? (
-                ""
-              ) : (
-                <p className="text-red-500 text-sm">* Title is mandatory</p>
-              )}
+                className="border px-3 py-[0.3em] rounded-[20px] focus:outline-none focus:border-green-500 w-full noscrollbar"
+              ></textarea>
             </div>
-            <textarea
-              rows="2"
-              placeholder="Description"
-              value={editedTodo.description}
-              onChange={(e) => {
-                setEditedTodo({ ...editedTodo, description: e.target.value });
-              }}
-              className="border px-3 py-[0.3em] rounded-[20px] focus:outline-none focus:border-green-500 w-full noscrollbar"
-            ></textarea>
 
-            <div className="flex gap-3">
-              <button
-                type="submit"
-                className="bg-blue-500 text-white hover:bg-blue-600 py-[0.3em] px-5 rounded-full min-w-fit w-[7em]"
-              >
-                Update
-              </button>
-              <button
-                onClick={() => setIsEditModeOn(!isEditModeOn)}
-                className="bg-orange-600 text-white hover:bg-orange-700 py-[0.3em] px-5 rounded-full min-w-fit w-[7em]"
-              >
-                Keep Existing
-              </button>
-            </div>
-          </form>
+            <RxCross2
+              title="Remove"
+              className="my-auto min-w-4 text-[1.2em] text-gray-400 hover:text-red-500 hover:scale-[1.2] duration-300"
+              onClick={(e) => {
+                e.stopPropagation()
+                dispatch(removeTodo(todo.createdAt));
+              }}
+            />
+          </div>
         ) : (
           // for displaying a todo
-          <div className="w-full" onClick={() => setIsEditModeOn(!isEditModeOn)}>
-            <div className="flex">
-              <div
-                className={`font-semibold ${
+          <div
+            className="w-full flex justify-between"
+            // onClick={() => setIsEditModeOn(!isEditModeOn)}
+          >
+            <div>
+              <div className="flex">
+                <div
+                  className={`font-semibold ${
+                    todo.isCompleted ? "line-through" : ""
+                  }`}
+                >
+                  {todo.title}
+                </div>
+                <div className="italic text-xs text-gray-600 ml-4">
+                  {/* ({day}/{month}/{year}) */}
+                  {date.toLocaleDateString()}
+                </div>
+
+                <div className="italic text-xs text-gray-600 ml-4">
+                  {/* ({day}/{month}/{year}) */}
+                  {date.toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: true,
+                  })}
+                </div>
+              </div>
+              <p
+                className={`italic text-gray-600 ${
                   todo.isCompleted ? "line-through" : ""
-                }`}
+                } ${todo.description ? "" : "invisible"}`}
               >
-                {todo.title}
-              </div>
-              <div className="italic text-xs text-gray-600 ml-4">
-                {/* ({day}/{month}/{year}) */}
-                {date.toLocaleDateString()}
-              </div>
-
-              <div className="italic text-xs text-gray-600 ml-4">
-                {/* ({day}/{month}/{year}) */}
-                 {date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}
-              </div>
+                {todo.description
+                  ? formattedDescription.map((jsx) => jsx)
+                  : "No description"}
+              </p>
             </div>
-            <p
-              className={`italic text-gray-600 ${
-                todo.isCompleted ? "line-through" : ""
-              } ${todo.description ? "" : "invisible"}`}
-            >
-              {todo.description ? (
-                  formattedDescription.map((jsx) => (jsx))
-                ) : "No description"}
 
-            </p>
+            <RxCross2
+              title="Remove"
+              className="my-auto min-w-4 text-[1.2em] text-gray-400 hover:text-red-500 hover:scale-[1.2] duration-300"
+              onClick={(e) => {
+                e.stopPropagation()
+                dispatch(removeTodo(todo.createdAt));
+              }}
+            />
           </div>
         )}
       </div>
-
-      <RxCross2
-        title="Remove"
-        className="my-auto min-w-4 text-[1.2em] text-gray-400 hover:text-red-500 hover:scale-[1.2] duration-300"
-        onClick={() => dispatch(removeTodo(todo.createdAt))}
-      />
     </div>
   );
 };
